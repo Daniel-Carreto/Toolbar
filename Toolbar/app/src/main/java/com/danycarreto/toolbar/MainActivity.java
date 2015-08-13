359465fbec9cdb621436b2e3328fc990b684dc28
Daniel Castillo Carreto <dany7carreto@gmail.com>
2015-08-12 23:25:57 -0500

    Simple Toolbar

---
+++ Toolbar/app/src/main/java/com/danycarreto/toolbar/MainActivity.java
@@ -0,0 +1,23 @@
+package com.danycarreto.toolbar;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbarPrincipal);
+        setSupportActionBar(toolbar);
+        setTitle("Titulo");
+    }
+
+}
